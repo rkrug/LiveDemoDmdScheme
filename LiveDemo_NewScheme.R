5ca77fd0e6561a39095cc9f8e31fa6095a104680
Rainer Krug <Rainer@krugs.de>
2019-08-29 14:27:20 +0200

    Initial Commint

---
+++ LiveDemo_NewScheme.R
@@ -0,0 +1,98 @@
+
+# Install Package dmdScheme -----------------------------------------------
+
+
+## install.packages("remotes")
+## remotes::install_github("Exp-Micro-Ecol-Hub/dmdScheme", ref = "shinyapp", build_opts = "")
+
+## later, it should as aeasy as:
+## install.packages("dmdScheme")
+
+
+# Load Package ------------------------------------------------------------
+
+
+library(dmdScheme)
+
+
+# View Scheme Definition --------------------------------------------------
+
+
+open_new_spreadsheet("dmdScheme", keepData = TRUE)
+
+
+# Create and Open new Scheme for Definition ------------------------------------------
+
+
+open_new_spreadsheet("dmdScheme", file = "newScheme.xlsx", keepData = TRUE, format = TRUE)
+
+
+# Edit the newScheme in Excel ---------------------------------------------
+
+
+## Excel
+## Change name to theNewScheme
+## Change Version to v0.1.0
+
+
+# Create new Scheme Package -----------------------------------------------
+
+
+dev_make_new_scheme(schemeDefinition = 'newScheme.xlsx', path = '.')
+
+
+
+# Edit theNewScheme -------------------------------------------------------
+
+
+setwd("theNewScheme")
+devtools::load_all(".")
+
+## Edits in the Excel file
+
+dev_update_from_new_sheet("./../newScheme.xlsx")
+
+
+# Build Package -----------------------------------------------------------
+
+
+setwd("..")
+
+devtools::build("./theNewScheme")
+
+
+# Restart R ---------------------------------------------------------------
+
+.rs.restartR()
+
+# Load theNewScheme -------------------------------------------------------
+
+
+library("theNewScheme")
+
+
+# Install theNewScheme ----------------------------------------------------
+
+
+install.packages( pkgs = "./theNewScheme_1.0.tar.gz", repos = NULL)
+
+
+# Load theNewScheme -------------------------------------------------------
+
+
+library("theNewScheme")
+
+
+# Use theNewScheme --------------------------------------------------------
+
+
+dmdScheme_versions()
+
+dmdScheme_versions("theNewScheme")
+
+open_new_spreadsheet("theNewScheme")
+
+
+# Questions ---------------------------------------------------------------
+
+
